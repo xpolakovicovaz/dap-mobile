b7305a607f0174670761c66ee98193a8cd82143b
xpolakovicovaz <zdenka.polakovicova@gmail.com>
2021-11-18 18:49:33 +0100

    not working stuff before package change

--- app/views/MainScreen.js
+++ app/views/MainScreen.js
@@ -1,23 +1,74 @@
 import React from 'react';
-import {StyleSheet, View, Text, Pressable, Image} from 'react-native'
+import {StyleSheet, View, Text, Pressable, Image, Dimensions} from 'react-native'
 
 
 import colors from '../global/colors';
-import {InitSetting} from '../db/dbController';
+import {InitSetting, GetDb, LastEnd, LastStart,PeriodLength, CycleLength,OvulationStart} from '../db/dbController';
 
 
-function MainScreen({navigation}) {
 
-    console.log("not go to init settings33");
+const MainScreen= ({navigation})=> {
+    const [items, setItems] = React.useState(null);
+    console.log("main screen - open ");
+    let number = 28;
+    if (LastStart < new Date())
+    number = GetRemindingDays();
    let a =  InitSetting();
+   console.log("db init ok ");
+   let db = GetDb();
+   React.useEffect(()=>{
+    db.transaction((tx)=>{
+        tx.executeSql("select * from sett",//sql
+            [], //sql args
+            (_, { rows:{ _array} }) =>{//callback function
+             //   console.log('db data res ------>', _array)
+                _array.map(({id, value})=> 
+                {
+                    switch(id)
+                    {
+                        case "cycle_length":
+                            cycle_length = value; CycleLength(value); break;
+                        case "ovulation_start":
+                            ovulation_start = value; OvulationStart(value);break;
+                        case "period_length":
+                            period_length = value;PeriodLength(value); break;
+                    }
+                });
+                console.log("cycle_length " + cycle_length);
+                console.log("ovulation_start " + ovulation_start);
+                console.log("period_length " + period_length);
+                //setItems();
+            },
+            error=>{//error function
+                console.log("ERROR - openDatabase kkk - "+ error);
+                //HandleDbProblem(error);
+            },
+  
+        );  
+        tx.executeSql("select date from day where p_start = 1 order by date desc limit 1",
+        [],
+        (tr, { rows:{ _array} })=> { _array.map(({date})=>{console.log("success - last_start - " +date);LastStart(date)})},
+        (tr, error)=>{console.log("ERROR - last_start - "+ error);}
+        );    
+        tx.executeSql("select date from day where p_end = 1 order by date desc limit 1",
+        [],
+        (tr, {rows:{array}})=> {_array.map(({date})=>{console.log("success - last end - " + date);LastEnd(date)})},
+        (tr, error)=>{console.log("ERROR - last_start - "+ error);}
+        );      
+    },          
+    (error) => HandleDbProblem(error),
+    setItems())
+});
+console.log("loading - end")
+
 
     console.log("MainScreen start");
     return (
         <View style={styles.background}>
             <View style={styles.textBox}>
-                <Text style={styles.smallText}>este</Text>
-                <Text style={styles.bigText}>15</Text>
-                <Text style={styles.smallText}>dni</Text>
+                <Text style={styles.smallText}>Ešte</Text>
+                <Text style={styles.bigText}>{number}</Text>
+                <Text style={styles.smallText}>{GetDayText(number)}</Text>
             </View>
             <View style={styles.buttonBox}>
                 <Pressable style={styles.buttonS} onPress={()=>navigation.navigate("Settings")}>
@@ -30,6 +81,22 @@ function MainScreen({navigation}) {
         </View>
     );
 }
+
+function GetDayText (number)
+{
+    switch(number)
+    {
+        case 1: return "deň";
+        case 2:
+        case 3:
+        case 4: return "dni";
+        default: return "dní";
+    }
+}
+function GetRemindingDays()
+{
+    
+} 
 
 const styles = StyleSheet.create({
 background:{
