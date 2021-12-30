a4aeeca14def6d4e0dd6552a2fe32e431221f03e
xpolakovicovaz <zdenka.polakovicova@gmail.com>
2021-12-30 20:03:18 +0100

    calendar element style

--- app/global/colors.js
+++ app/global/colors.js
@@ -3,6 +3,7 @@
      red:"#ff0000",
      green:"#109912",
      grey:"#bbbbbb",
+     grey_inactivee:"#707070",
      grey_select:"#aaaaaa",
      grey_important:"#505050",
      grey_hover:"#888888",
